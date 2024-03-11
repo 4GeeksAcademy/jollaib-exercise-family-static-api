afc9e9ce09e5414861fbcae91ba1463c693845ca
jollaib <56800825+jollaib@users.noreply.github.com>
2024-03-11 20:04:50 +0000

    tarea terminada

--- src/app.py
+++ src/app.py
@@ -30,13 +30,36 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    return jsonify(members), 200
+
+@app.route('/member', methods = ['POST'])
+def add_member():
+    body = request.get_json()
+    print(body)
+    if "id" not in body:
+        id = jackson_family._generateId()
+    else: 
+        id = body["id"]
+    new_member = {
+        "id":  id ,#jackson_family._generateId(),
+        "first_name": body["first_name"],
+        "last_name": jackson_family.last_name,
+        "age": body["age"],
+        "lucky_numbers": body["lucky_numbers"]
     }
+    jackson_family.add_member(new_member)
+    return jsonify("add member completed"), 200
 
+@app.route('/member/<int:id>', methods = ['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods = ['DELETE'])
+def delete_member(id):
+    jackson_family.delete_member(id)
+    return jsonify({"done": True}), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
